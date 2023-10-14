97e86307345474957c7f3e85cec5bbb4390e3562
sayedkhaledd <sayedkhaledsa@gmail.com>
2023-10-14 03:33:59 +0300

    initial structure for base

---
+++ src/main/java/com/example/backendcoreservice/BackendCoreServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendcoreservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCoreServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendCoreServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backendcoreservice/BackendCoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendcoreservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCoreServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
