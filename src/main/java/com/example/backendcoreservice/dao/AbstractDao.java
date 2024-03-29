aa88e8d973eb351763940a3ccd27bc13a0546680
sayedkhaledd <sayedkhaledsa@gmail.com>
2024-03-29 09:52:17 +0200

    Added default methods for creation and find

--- src/main/java/com/example/backendcoreservice/dao/AbstractDao.java
+++ src/main/java/com/example/backendcoreservice/dao/AbstractDao.java
@@ -3,7 +3,13 @@ package com.example.backendcoreservice.dao;
 import com.example.backendcoreservice.model.AbstractEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AbstractDao<E extends AbstractEntity, T extends JpaRepository<E, Long>> {
 
     T getRepo();
+
+    default Optional<E> findById(Long id) {
+        return getRepo().findById(id);
+    }
 }
--- src/main/java/com/example/backendcoreservice/service/AbstractService.java
+++ src/main/java/com/example/backendcoreservice/service/AbstractService.java
@@ -4,10 +4,29 @@ import com.example.backendcoreservice.dao.AbstractDao;
 import com.example.backendcoreservice.dto.AbstractDto;
 import com.example.backendcoreservice.model.AbstractEntity;
 import com.example.backendcoreservice.transformer.AbstractTransformer;
+import jakarta.persistence.EntityNotFoundException;
+
+import java.util.Optional;
 
 public interface AbstractService<E extends AbstractEntity, T extends AbstractDto, K extends AbstractTransformer, L extends AbstractDao> {
 
     K getTransformer();
 
     L getDao();
+
+    default T findById(Long id) {
+        Optional<E> dto = getDao().findById(id);
+        if (dto.isPresent()) {
+            return (T) getTransformer().transformEntityToDto(dto.get());
+        }
+        throw new EntityNotFoundException("Entity not found");
+    }
+
+    default T create(T dto) {
+        if (dto.getId() != null && findById(dto.getId()) != null)
+            throw new EntityNotFoundException(String.format("Entity with id %s already exists", dto.getId()));
+
+        E entity = (E) getTransformer().transformDtoToEntity(dto);
+        return (T) getTransformer().transformEntityToDto((E) getDao().getRepo().save(entity));
+    }
 }
