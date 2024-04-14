4523ef5ad6940fa8f26439691bc0eb7de70d1f35
sayedkhaledd <sayedkhaledsa@gmail.com>
2024-04-14 15:16:10 +0200

    Add @EqualsAndHashCode for AbstractDto

--- src/main/java/com/example/backendcoreservice/dto/AbstractDto.java
+++ src/main/java/com/example/backendcoreservice/dto/AbstractDto.java
@@ -1,9 +1,6 @@
 package com.example.backendcoreservice.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
@@ -11,6 +8,7 @@ import java.time.LocalDateTime;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode
 public class AbstractDto {
     private Long id;
 
