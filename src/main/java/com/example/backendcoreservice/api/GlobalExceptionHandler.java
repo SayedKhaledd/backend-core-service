b2515e6e7b72b51cf2b98b6eadd3c366ae357b1a
sayedkhaledd <sayedkhaledsa@gmail.com>
2024-10-17 08:27:57 +0300

    update

--- src/main/java/com/example/backendcoreservice/api/GlobalExceptionHandler.java
+++ src/main/java/com/example/backendcoreservice/api/GlobalExceptionHandler.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.Arrays;
+
 @AllArgsConstructor
 @Slf4j
 @RestControllerAdvice()
@@ -18,26 +20,26 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = EntityNotFoundException.class)
     public ResponseEntity handleException(EntityNotFoundException e) {
-        log.info("Entity not found exception: {}", e.getMessage());
+        log.error("Entity not found exception: {}", e);
         return new ResponseEntity<>(apiResponseBuilder.buildFailureResponse(e.getMessage(), 404), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(value = CustomException.class)
     public ResponseEntity handleException(CustomException e) {
-        log.error("Internal server error: {}", e.getMessage());
+        log.error("Internal server error: {}", e);
         return new ResponseEntity<>(apiResponseBuilder.buildFailureResponse(e.getMessage(), 500), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(value = RuntimeException.class)
     public ResponseEntity handleException(RuntimeException e) {
-        log.error("Internal server error: {}", e.getMessage());
+        log.error("Internal server error: {}", e);
         return new ResponseEntity<>(apiResponseBuilder.buildFailureResponse(e.getMessage(), 500), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
     public ResponseEntity handleException(MethodArgumentNotValidException e) {
-        log.error("MethodArgumentNotValidException: {}", e.getMessage());
-        return new ResponseEntity<>(apiResponseBuilder.buildFailureResponse(e.getMessage(), 400), HttpStatus.BAD_REQUEST);
+        log.error("MethodArgumentNotValidException: {}", e);
+        return new ResponseEntity<>(apiResponseBuilder.buildFailureResponse(Arrays.toString(e.getDetailMessageArguments()), 400), HttpStatus.BAD_REQUEST);
     }
 
 }
